9284628a5465d04824b451afe8fa6bed5b2689d5
knqyf263 <knqyf263@gmail.com>
2017-04-01 21:40:03 +0900

    v0.2.1

--- pet.rb
+++ pet.rb
@@ -1,9 +1,9 @@
 class Pet < Formula
   desc "Simple command-line snippet manager"
   homepage "https://github.com/knqyf263/pet"
-  version "0.2.0"
+  version "0.2.1"
   url "https://github.com/knqyf263/pet/releases/download/v#{version}/pet_#{version}_darwin_amd64.zip"
-  sha256 "7043fbbbc5b97323b4682c32dab536a6537012c7b0051369ad34b110826b03d5"
+  sha256 "637f68a640f91b46d75e8b787f316342831dc32cd3dcb3008b0f437c7c5a59a7"
 
   depends_on "peco"
 
