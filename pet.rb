09376ff3ef2cbc3b6c3ea98f2971ec1c7abd0b5c
knqyf263 <knqyf263@gmail.com>
2017-05-30 21:36:39 +0900

    v0.2.3 and Install zsh_completion

--- pet.rb
+++ pet.rb
@@ -1,14 +1,15 @@
 class Pet < Formula
   desc "Simple command-line snippet manager"
   homepage "https://github.com/knqyf263/pet"
-  version "0.2.2"
+  version "0.2.3"
   url "https://github.com/knqyf263/pet/releases/download/v#{version}/pet_#{version}_darwin_amd64.zip"
-  sha256 "2b4b2c91fe76f395935ff16f22f9a0787c61f35cb869b343ceff5f41d568c63c"
+  sha256 "00cc6c762c477f0973508bbcf51286d35017345963f48d7bd7418aaccdd92cb0"
 
   depends_on "peco"
 
   def install
     bin.install Dir['pet']
+    zsh_completion.install "misc/completions/zsh/_pet"
   end
 
   test do
