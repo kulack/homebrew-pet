87e1ba390e80505e52bcffe1b80cc5a63da0ce31
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-14 22:15:22 +0900

    Brew formula update for pet version v0.3.2

--- pet.rb
+++ pet.rb
@@ -1,9 +1,9 @@
 class Pet < Formula
   desc "Simple command-line snippet manager"
   homepage "https://github.com/knqyf263/pet"
-  url "https://github.com/knqyf263/pet/releases/download/v0.3.1/pet_0.3.1_darwin_amd64.tar.gz"
-  version "0.3.1"
-  sha256 "d67b55190343fd0957458eec9b97328dd2c155bae52eca21b01543f20873ca7a"
+  url "https://github.com/knqyf263/pet/releases/download/v0.3.2/pet_0.3.2_darwin_amd64.tar.gz"
+  version "0.3.2"
+  sha256 "a15ae670ab17a1a1253da4451fb57c5756874db5f931438f23e95866d8f44fc1"
   
   depends_on "fzf"
 
