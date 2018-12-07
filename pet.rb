3d732ad286d5203c2a515bdb9358f56bb9602fbf
Fred A Kulack <kulack@users.noreply.github.com>
2018-12-07 16:34:33 -0600

    Initial change to kulack packaging

--- pet.rb
+++ pet.rb
@@ -1,9 +1,9 @@
 class Pet < Formula
   desc "Simple command-line snippet manager"
-  homepage "https://github.com/knqyf263/pet"
-  url "https://github.com/knqyf263/pet/releases/download/v0.3.2/pet_0.3.2_darwin_amd64.tar.gz"
-  version "0.3.2"
-  sha256 "a15ae670ab17a1a1253da4451fb57c5756874db5f931438f23e95866d8f44fc1"
+  homepage "https://github.com/kulack/pet"
+  url "https://github.com/kulack/pet/releases/download/v0.3.3/pet_0.3.3_darwin_amd64.tar.gz"
+  version "0.3.3"
+  sha256 ""
   
   depends_on "fzf"
 
